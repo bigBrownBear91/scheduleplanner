87199b7d7f2ee0c667077882b4d086d9522baa02
bigBrownBear91 <Radovan21>
2020-08-11 13:45:55 +0200

    Add SQLALCHEMY_DATABASE_URI to config and take the value from environment variables

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ import secrets
 SESSION_TYPE = 'filesystem'
 SECRETE_KEY = os.getenv('SECRETE_KEY')
 WTF_CSRF_SECRET_KEY = secrets.token_bytes()
+SQLALCHEM_DATABASE_URI = os.getenv('SQLALCHEM_DATABASE_URI')
